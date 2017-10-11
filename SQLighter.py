d75085e2faf6710052642597efc336884d555241
Yury Andreev <yandreev92@yandex.com>
2017-10-11 20:25:10 +0300

    изменено отображение данных при ответе, исправлено отображение кастомной клавиатуры

--- SQLighter.py
+++ SQLighter.py
@@ -8,16 +8,14 @@ class SQLighter:
         self.connection = sqlite3.connect(database)
         self.cursor = self.connection.cursor()
 
-
     def select_all(self):
-        """ Получаем всё расписание из БД, имя
-            талицы передаётся аргументом в метод
-            """
+        """Получаем всё расписание из БД, имя
+           талицы передаётся аргументом в метод
+           """
         with self.connection:
             query = 'SELECT date, time, subject, room, lecturer FROM training_schedule'
             self.cursor.execute(query)
             return self.cursor.fetchall()
-
 
     def get_shedule_on_day(self, date):
         """ Принимаем на вход дату, тащим из ДБ расписание на эту дату """
@@ -30,7 +28,6 @@ class SQLighter:
         with self.connection:
             self.cursor.execute('SELECT date FROM training_schedule')
         return self.cursor.fetchall()
-
 
     def close(self):
         """ Закрываем соединение с БД """
